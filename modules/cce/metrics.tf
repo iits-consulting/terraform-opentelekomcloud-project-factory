9ebd602be2fed533273cccf04fd3b05a7ec34584
Tim Delbrügger <tim.delbruegger@impresssol.de>
2021-06-07 10:58:15 +0200

    metrics for cce 1.19.x

--- modules/cce/metrics.tf
+++ modules/cce/metrics.tf
@@ -1,6 +1,6 @@
 resource opentelekomcloud_cce_addon_v3 metrics {
   template_name    = "metrics-server"
-  template_version = "1.0.3"
+  template_version = "1.0.6"
   cluster_id       = opentelekomcloud_cce_cluster_v3.cluster.id
 
   values {
