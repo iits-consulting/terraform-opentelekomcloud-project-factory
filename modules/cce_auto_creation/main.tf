f119dbceb772e552373682fba30b3c1fbeea6e33
Tim Delbrügger <tim.delbruegger@impresssol.de>
2021-06-11 15:28:09 +0200

    fix wrong for_each

--- modules/cce_auto_creation/main.tf
+++ modules/cce_auto_creation/main.tf
@@ -2,13 +2,9 @@ resource "opentelekomcloud_identity_agency_v3" "enable_cce_auto_creation" {
   name                  = "cce_admin_trust_${var.project}"
   description           = "Created by Terraform to auto create cce"
   delegated_domain_name = "op_svc_cce"
-  dynamic "project_role" {
-    for_each = var.project
-    content {
-      project = project_role.value
-      roles   = [
-        "Tenant Administrator"
-      ]
-    }
+  project_role {
+    project = var.project
+    roles   = [
+      "Tenant Administrator"]
   }
 }
