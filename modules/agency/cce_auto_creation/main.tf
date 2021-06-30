bca6f815087b271b15b34b3c45777efbb7a15fb4
Victor Getz <sd>
2021-06-30 11:05:11 +0200

    formatting

--- modules/agency/cce_auto_creation/main.tf
+++ modules/agency/cce_auto_creation/main.tf
@@ -1,6 +1,6 @@
 resource "opentelekomcloud_identity_agency_v3" "enable_cce_auto_creation" {
-  name = "cce_admin_trust"
-  description = "Created by Terraform to auto create cce"
+  name                  = "cce_admin_trust"
+  description           = "Created by Terraform to auto create cce"
   delegated_domain_name = "op_svc_cce"
   dynamic "project_role" {
     for_each = var.projects
