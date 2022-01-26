3ad59286b371504f0ff16ad35f906c36f4af21a8
Can Aykin <can.aykin@iits-consulting.de>
2022-01-26 16:19:22 +0100

    add description to subnet_id for loadbalancer module add tf_state_backend script for terraform s3 backend preperation (SSE KMS default key enabled)

--- tf_state_backend/main.tf
+++ tf_state_backend/main.tf
@@ -36,9 +36,11 @@ output "backend_config" {
   value = <<EOT
     backend "s3" {
       bucket = "${opentelekomcloud_obs_bucket.tf_remote_state.bucket}"
+      kms_key_id = "arn:aws:kms:eu-de:${opentelekomcloud_kms_key_v1.tf_remote_state_bucket_kms_key.domain_id}:key/${opentelekomcloud_kms_key_v1.tf_remote_state_bucket_kms_key.id}"
       key = "tfstate"
       region = "${opentelekomcloud_obs_bucket.tf_remote_state.region}"
       endpoint = "obs.eu-de.otc.t-systems.com"
+      encrypt = true
       skip_region_validation = true
       skip_credentials_validation = true
     }
