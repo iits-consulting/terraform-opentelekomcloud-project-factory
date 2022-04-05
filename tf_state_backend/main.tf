853be77bddfe7701fc23cba65c2954d73cff94ef
Can Aykin <can.aykin@iits-consulting.de>
2022-04-05 15:49:21 +0200

    bugfix tf_state_backend (endpoint supports eu-nl now)

--- tf_state_backend/main.tf
+++ tf_state_backend/main.tf
@@ -28,7 +28,7 @@ locals {
 }
 
 provider "opentelekomcloud" {
-  auth_url    = "https://iam.eu-de.otc.t-systems.com/v3"
+  auth_url    = "https://iam.${var.region}.otc.t-systems.com/v3"
   tenant_name = var.region
 }
 
