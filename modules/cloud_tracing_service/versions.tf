56f8b6093f92bab0eb01ed3c727ce0b3bf1afbbc
canaykin <canaykin@users.noreply.github.com>
2022-04-12 17:13:52 +0000

    terraform fmt

--- modules/cloud_tracing_service/versions.tf
+++ modules/cloud_tracing_service/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     opentelekomcloud = {
-      source = "opentelekomcloud/opentelekomcloud"
+      source  = "opentelekomcloud/opentelekomcloud"
       version = ">=1.29.0"
     }
   }
