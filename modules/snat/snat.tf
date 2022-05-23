4a1ca4b85d9551c1361c1e668ae3015aa1279a13
Can Aykin <can.aykin@iits-consulting.de>
2022-05-23 14:59:56 +0200

    major bugfixes for SNAT module

--- modules/snat/snat.tf
+++ modules/snat/snat.tf
@@ -19,7 +19,7 @@ resource "opentelekomcloud_vpc_eip_v1" "nat_ip" {
 }
 
 resource "opentelekomcloud_nat_snat_rule_v2" "snat_subnet" {
-  for_each       = var.network_ids == [] ? toset([var.subnet_id]) : var.network_ids
+  for_each       = length(var.network_ids) > 0 ? var.network_ids : toset([var.subnet_id])
   nat_gateway_id = opentelekomcloud_nat_gateway_v2.nat.id
   source_type    = 0
   network_id     = each.key
@@ -30,6 +30,6 @@ resource "opentelekomcloud_nat_snat_rule_v2" "snat_cidr" {
   for_each       = var.network_cidrs
   nat_gateway_id = opentelekomcloud_nat_gateway_v2.nat.id
   source_type    = 0
-  network_id     = each.key
+  cidr           = each.key
   floating_ip_id = opentelekomcloud_vpc_eip_v1.nat_ip.id
 }
