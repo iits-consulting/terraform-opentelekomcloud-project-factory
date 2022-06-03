1d0e05cdc0ef6789d65968a9d2a32d766cbceef1
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-06-03 16:00:47 +0300

    Save backward compatibility

--- modules/cce/output.tf
+++ modules/cce/output.tf
@@ -24,10 +24,10 @@ output "cluster_name" {
   value = opentelekomcloud_cce_cluster_v3.cluster.name
 }
 
-output "local_kubeconfig" {
+output "kubeconfig" {
   value = local.kubectl_config
 }
 
-output "kubeconfig" {
+output "kubeconfig_json" {
   value = jsondecode(data.opentelekomcloud_cce_cluster_kubeconfig_v3.kubeconfig.kubeconfig)
 }
