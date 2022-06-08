3b1ea69783765d31841cf97360e10a2642fec5cb
Can Aykin <can.aykin@iits-consulting.de>
2022-06-08 15:59:03 +0200

    reverted provider config changes for CTS. It should be operational. Need review.

--- modules/cloud_tracing_service/main.tf
+++ modules/cloud_tracing_service/main.tf
@@ -14,7 +14,6 @@ resource "opentelekomcloud_obs_bucket" "cloud_tracing_service" {
       days = var.cts_expiration_days
     }
   }
-  provider = opentelekomcloud.top_level
 }
 
 resource "opentelekomcloud_cts_tracker_v1" "cloud_tracing_service_tracker_v1" {
@@ -24,5 +23,4 @@ resource "opentelekomcloud_cts_tracker_v1" "cloud_tracing_service_tracker_v1" {
   is_send_all_key_operation = false     # Required: When the value is TRUE, operations cannot be left empty. Official DOCs are wrong.
   is_support_smn            = false     # "true" allows to notify authoritive users about activity, When the value is false, topic_id and operations can be left empty.
   project_name              = var.project_name
-  provider                  = opentelekomcloud.subproject
 }
