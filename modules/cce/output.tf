1ec84398e5b886706d68b5b1101acf3550029452
k11h.de <17837008+k11h-de@users.noreply.github.com>
2022-07-29 21:11:29 +0200

    Update modules/cce/output.tf
    Co-authored-by: canaykin <70644420+canaykin@users.noreply.github.com>

--- modules/cce/output.tf
+++ modules/cce/output.tf
@@ -2,7 +2,7 @@ output "cluster_lb_public_ip" {
   value = opentelekomcloud_vpc_eip_v1.cce_eip.publicip[0].ip_address
 }
 
-output "node_pool_id" {
+output "node_pool_ids" {
   value = opentelekomcloud_cce_node_pool_v3.cluster_node_pool[*].id
 }
 
