a29444b7feb3fee8d85d1fc7fef4b0cdf41ea46f
Can Aykin <can.aykin@iits-consulting.de>
2022-08-05 11:11:28 +0200

    Loadbalancer: add elb_private_ip to outputs

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -21,6 +21,10 @@ output "elb_id" {
   value = opentelekomcloud_lb_loadbalancer_v2.elb.id
 }
 
+output "elb_private_ip" {
+  value = opentelekomcloud_lb_loadbalancer_v2.elb.vip_address
+}
+
 output "elb_public_ip" {
   value = opentelekomcloud_vpc_eip_v1.ingress_eip.publicip[0].ip_address
 }
