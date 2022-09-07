f0fa75b06ed326bbf1ecfb2a18f705fa802b41af
canaykin <70644420+canaykin@users.noreply.github.com>
2022-09-07 18:42:08 +0200

    Update kubectl.tf
    Refactor kubeconfig handling:
    - external and internal parts of config are now separated
    - external part of config is concatenated when cluster is public
    - internal part of config is always included

--- modules/cce/kubectl.tf
+++ modules/cce/kubectl.tf
@@ -4,8 +4,42 @@ locals {
   kubectl_external_server            = try(opentelekomcloud_cce_cluster_v3.cluster.certificate_clusters[1].server, "")
   kubectl_internal_server            = opentelekomcloud_cce_cluster_v3.cluster.certificate_clusters[0].server
   cluster_certificate_authority_data = opentelekomcloud_cce_cluster_v3.cluster.certificate_clusters[0].certificate_authority_data
-  kubectl_config_raw = {
-    apiVersion = "v1"
+
+  kubectl_config_raw_internal = {
+    clusters = [
+      {
+        cluster = {
+          server                     = local.kubectl_internal_server
+          certificate-authority-data = local.cluster_certificate_authority_data
+        }
+        name = "${var.name}-cluster-internal"
+      },
+      {
+        cluster = {
+          insecure-skip-tls-verify = true
+          server                   = local.kubectl_internal_server
+        }
+        name = "${var.name}-cluster-insecure-internal"
+      },
+    ]
+    contexts = [
+      {
+        context = {
+          cluster = "${var.name}-cluster-internal"
+          user    = "terraform"
+        }
+        name = "${var.name}-internal"
+      },
+      {
+        context = {
+          cluster = "${var.name}-cluster-insecure-internal"
+          user    = "terraform"
+        }
+        name = "${var.name}-insecure-internal"
+      },
+    ]
+  }
+  kubectl_config_raw_external = {
     clusters = [
       {
         cluster = {
@@ -21,67 +55,6 @@ locals {
         }
         name = "${var.name}-cluster-insecure"
       },
-      {
-        cluster = {
-          server                     = local.kubectl_internal_server
-          certificate-authority-data = local.cluster_certificate_authority_data
-        }
-        name = "${var.name}-cluster-internal"
-      },
-    ]
-    contexts = [
-      {
-        context = {
-          cluster = "${var.name}-cluster"
-          user    = "terraform"
-        }
-        name = var.name
-      },
-      {
-        context = {
-          cluster = "${var.name}-cluster-insecure"
-          user    = "terraform"
-        }
-        name = "${var.name}-insecure"
-      },
-      {
-        context = {
-          cluster = "${var.name}-cluster-internal"
-          user    = "terraform"
-        }
-        name = "${var.name}-internal"
-      },
-    ]
-    current-context = var.name
-    kind            = "Config"
-    preferences     = {}
-    users = [
-      {
-        name = "terraform"
-        user = {
-          client-certificate-data = local.client_certificate_data
-          client-key-data         = local.client_key_data
-        }
-      },
-    ]
-  }
-  kubectl_config_raw_internal = {
-    apiVersion = "v1"
-    clusters = [
-      {
-        cluster = {
-          server                     = local.kubectl_internal_server
-          certificate-authority-data = local.cluster_certificate_authority_data
-        }
-        name = "${var.name}-cluster"
-      },
-      {
-        cluster = {
-          insecure-skip-tls-verify = true
-          server                   = local.kubectl_internal_server
-        }
-        name = "${var.name}-cluster-insecure"
-      },
     ]
     contexts = [
       {
@@ -99,7 +72,12 @@ locals {
         name = "${var.name}-insecure"
       },
     ]
-    current-context = var.name
+  }
+  kubectl_config_raw = {
+    apiVersion      = "v1"
+    clusters        = local.cluster_config.public_cluster ? concat(local.kubectl_config_raw_external.clusters, local.kubectl_config_raw_internal.clusters) : local.kubectl_config_raw_internal.clusters
+    contexts        = local.cluster_config.public_cluster ? concat(local.kubectl_config_raw_external.contexts, local.kubectl_config_raw_internal.contexts) : local.kubectl_config_raw_internal.contexts
+    current-context = local.cluster_config.public_cluster ? var.name : "${var.name}-internal"
     kind            = "Config"
     preferences     = {}
     users = [
@@ -112,6 +90,6 @@ locals {
       },
     ]
   }
-  kubectl_config_yaml = local.cluster_config.cluster_is_public ? yamlencode(local.kubectl_config_raw) : yamlencode(local.kubectl_config_raw_internal)
-  kubectl_config_json = local.cluster_config.cluster_is_public ? jsonencode(local.kubectl_config_raw) : jsonencode(local.kubectl_config_raw_internal)
+  kubectl_config_yaml = yamlencode(local.kubectl_config_raw)
+  kubectl_config_json = jsonencode(local.kubectl_config_raw)
 }
