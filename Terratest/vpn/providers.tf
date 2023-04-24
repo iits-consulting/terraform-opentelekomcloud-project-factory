29913a8333fa89642c28d93954a6b6106f4cd76e
Philip Siemer <philip.siemer@iits-consulting.de>
2023-04-24 16:27:28 +0200

    update provider tenant name

--- Terratest/vpn/providers.tf
+++ Terratest/vpn/providers.tf
@@ -2,7 +2,7 @@ provider "opentelekomcloud" {
   alias       = "nl"
   max_retries = 100
   auth_url    = "https://iam.eu-nl.otc.t-systems.com/v3"
-  tenant_name = "eu-nl_terratest"
+  tenant_name = "eu-nl_terratest-project-factory"
   region      = "eu-nl"
 }
 
@@ -10,6 +10,6 @@ provider "opentelekomcloud" {
   alias       = "de"
   max_retries = 100
   auth_url    = "https://iam.eu-de.otc.t-systems.com/v3"
-  tenant_name = "eu-de_terratest"
+  tenant_name = "eu-de_terratest-project-factory"
   region      = "eu-de"
 }
