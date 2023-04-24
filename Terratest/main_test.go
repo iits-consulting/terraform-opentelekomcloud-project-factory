a8282ee867420699825578d6768a703fa9ce2a7f
Philip Siemer <philip.siemer@iits-consulting.de>
2023-04-24 16:48:27 +0200

    changes moved to update_terratest_pipeline branch

--- Terratest/main_test.go
+++ Terratest/main_test.go
@@ -7,12 +7,40 @@ import (
 	"testing"
 )
 
-func TestModule(t *testing.T) {
-	context := os.Getenv("TF_VAR_context")
+func TestNetworkSetup(t *testing.T) {
+	context := "network"
 	performTerratest(t, context)
 }
 
+func TestCCE(t *testing.T) {
+	context := "cce"
+	performTerratest(t, context)
+}
+
+func TestPublicDNS(t *testing.T) {
+	context := "public_dns"
+	if os.Getenv("TF_VAR_region") == "eu-de" {
+		performTerratest(t, context)
+	} else {
+		fmt.Println("Skipping Test PublicDNS for Region", os.Getenv("TF_VAR_region"))
+	}
+}
+
+//FIXME Currently not working
+//func TestRDS(t *testing.T) {
+//	context := "rds"
+//	performTerratest(t, context)
+//}
+
+//FIXME Currently not working
+//func TestWAF(t *testing.T) {
+//	context := "waf"
+//	performTerratest(t, context)
+//}
+
 func performTerratest(t *testing.T, context string) {
+	os.Setenv("TF_VAR_context", context)
+
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: context,
 	})
