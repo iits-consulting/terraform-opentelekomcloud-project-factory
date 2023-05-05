01cb656b00ca9c63c59855db3ffde14f299b93cd
Denis Lavrushko <denis.lavrushko@iits-consulting.de>
2023-05-05 08:26:53 +0200

    fixing terratest for obs_restricted

--- Terratest/obs_restricted/main.tf
+++ Terratest/obs_restricted/main.tf
@@ -1,12 +1,14 @@
 module "obs_restricted_eu_nl" {
-  source = "../../modules/obs_restricted"
+  source      = "../../modules/obs_restricted"
+  bucket_name = var.bucket_name
   providers = {
     opentelekomcloud = opentelekomcloud.nl
   }
 }
 
 module "obs_restricted_eu_de" {
-  source = "../../modules/obs_restricted"
+  source      = "../../modules/obs_restricted"
+  bucket_name = var.bucket_name
   providers = {
     opentelekomcloud = opentelekomcloud.de
   }
