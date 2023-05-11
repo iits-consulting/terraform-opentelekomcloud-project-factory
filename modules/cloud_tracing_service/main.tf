d48aa30e5216daea46f66a32f9add6a90c717db4
Mattias Cockburn <mattias.cockburn@iits-consulting.de>
2023-05-11 15:12:02 +0200

    cloud_tracing_service: fix ups for current provider
    The current releases of the provider do not support most of the
    parameters used in this module. This commit removes unsupported flags
    and adds a new one to enable trace analysis.

--- modules/cloud_tracing_service/main.tf
+++ modules/cloud_tracing_service/main.tf
@@ -17,13 +17,7 @@ resource "opentelekomcloud_obs_bucket" "cloud_tracing_service" {
 }
 
 resource "opentelekomcloud_cts_tracker_v1" "cloud_tracing_service_tracker_v1" {
-  bucket_name               = opentelekomcloud_obs_bucket.cloud_tracing_service.bucket
-  file_prefix_name          = var.file_prefix
-  status                    = "enabled" # when updating the value can be enabled or disabled.
-  is_send_all_key_operation = false     # Required: When the value is TRUE, operations cannot be left empty. Official DOCs are wrong.
-  is_support_smn            = false     # "true" allows to notify authoritive users about activity, When the value is false, topic_id and operations can be left empty.
-  project_name              = var.project_name
-  lifecycle {
-    ignore_changes = [project_name] // This is to prevent deletion of the tracker as OTC seems to not record this parameter
-  }
+  bucket_name      = opentelekomcloud_obs_bucket.cloud_tracing_service.bucket
+  file_prefix_name = var.file_prefix
+  is_lts_enabled   = var.enable_trace_analysis
 }
--- modules/cloud_tracing_service/variables.tf
+++ modules/cloud_tracing_service/variables.tf
@@ -17,3 +17,9 @@ variable "cts_expiration_days" {
   default     = 180
   description = "How long should the data be preserved within the OBS bucket (in days). default: 180"
 }
+
+variable "enable_trace_analysis" {
+  type        = bool
+  default     = false
+  description = "Enables/disable trace analysis (LTS) for the tracker. Default: false"
+}
