2b2e3140027dab78d41b249c8a04225884ba1efb
Victor Getz <sd>
2024-01-16 11:07:59 +0100

    #93 Add oidc module, rename sso to saml - add eu-nl check and update README.md

--- modules/keycloak_sso_oidc/eu-nl-check.tf
+++
@@ -1,9 +0,0 @@
-data "opentelekomcloud_identity_project_v3" "current" {}
-
-resource "errorcheck_is_valid" "provider_project_constraint" {
-  name = "Check if the provider is not eu-nl."
-  test = {
-    assert        = data.opentelekomcloud_identity_project_v3.current.region != "eu-nl"
-    error_message = "ERROR! This module can not run with eu-nl since the api only works for eu-de. Just switch the region for the provider it will work for eu-de and eu-nl."
-  }
-}
