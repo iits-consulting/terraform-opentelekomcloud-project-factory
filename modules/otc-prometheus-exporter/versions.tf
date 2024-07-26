5a2e97f1ffe35af887c006de04f9e686fb56bd4e
erjondibranica <erjon.dibrani@iits-consulting.de>
2024-07-26 11:24:57 -0400

    remove unnecessary provider

--- modules/otc-prometheus-exporter/versions.tf
+++ modules/otc-prometheus-exporter/versions.tf
@@ -1,13 +1,11 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
     helm = {
       source = "hashicorp/helm"
     }
     opentelekomcloud = {
       source = "opentelekomcloud/opentelekomcloud"
+      
     }
   }
 }
